e2a23f5d4143decaacbc7cb68b23ed35acbd7c0d
ibrahim2337 <ibrahimkhalilullah680@gmail.com>
2023-05-04 04:04:50 +0600

    data load

--- index.js
+++ index.js
@@ -7,12 +7,19 @@ app.use(cors())
 
 const recipes = require("./data/recipe.json")
 
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.get("/recipes", (req, res) => {
   res.send(recipes);
+});
+
+app.get("/recipes/:id", (req, res) => {
+  const id = req.params.id;
+  const singleChef = recipes.find((c) => c.id === id);
+  res.send(singleChef);
 });
 
 app.listen(port, () => {
